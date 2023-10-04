63f3516d65f7f61c1291e33c4c9890df4a6567cb
batman <ee19b030@iittp.ac.in>
2023-10-04 15:06:49 +0530

    adding images wip

--- product-management/src/components/dashboard/Dashboard.js
+++ product-management/src/components/dashboard/Dashboard.js
@@ -1,8 +1,19 @@
 export const Dashboard = () => {
   return <div>
     This is Dashboard component.
-    <div>
-      <img class="100vh" src="product-management/src/components/dashboard/aston_martindb12.jpeg" alt="DB12"/>
+    <div  className="d-flex flex-row">
+      <div>
+      <img src="product-management/src/components/dashboard/aston_martindb12.jpeg" alt=""/>
+      <p>Model: DB12</p>
+      <p>Price: $100,000</p>
+      </div>
+      <div>
+        <img src="product-management/src/components/dashboard/aston_martindb12.jpeg" alt=""/>
+        <p>Model: Cobra</p>
+        <p>Price: $100,000</p>
+      </div>
+      
+
     </div>
   </div>;
   
