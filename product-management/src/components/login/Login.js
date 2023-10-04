ab2aa43f8af1185b4523b8020b288f43e0267ad6
nagesh <nageshyour@gmail.com>
2023-10-04 22:19:17 +0530

    added login form with usestate

--- product-management/src/components/login/Login.js
+++ product-management/src/components/login/Login.js
@@ -1,7 +1,33 @@
+import React, { useState } from "react";
+
 export const Login = () => {
-  return <div>Login Component...</div>;
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserIdChange = (event) => {
+    const value = event.target.value;
+    setUserId(value);
+  };
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleSubmit = () => {
+    console.log(userId, password);
+  };
+
+  return (
+    <div>
+      <div>
+        <label for="user-id">UserId: </label>
+        <input type="text" onChange={handleUserIdChange} id="user-id" />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" onChange={handlePasswordChange} id="password" />
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 };
-//Sakshi
-//create a login form with user name and password, and remember me chekbox.
-// and add cancel and submit button
-//it should also have register list below the form
