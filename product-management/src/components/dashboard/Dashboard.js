d423f9d4f9f2e0bec32f351b5c90a71485dd097d
batman <ee19b030@iittp.ac.in>
2023-10-07 22:11:34 +0530

    resolved conflicts

--- product-management/src/components/dashboard/Dashboard.js
+++ product-management/src/components/dashboard/Dashboard.js
@@ -1,45 +1,38 @@
+import { useState, useEffect } from "react";
 import "./Dashboard.css";
 export const Dashboard = () => {
-  const products = [
-    { image: "aston_martindb12.jpeg", model: "DB12", price: 100000 },
-    {
-      image: "1965-shelby-cobra-superformance.jpeg",
-      model: "Cobra",
-      price: 900000,
-    },
-    {
-      image: "1965-shelby-cobra-superformance.jpeg",
-      model: "ZX",
-      price: 80000,
-    },
-    {
-      image: "1965-shelby-cobra-superformance.jpeg",
-      model: "TVE",
-      price: 0,
-    },
-    {
-      image: "1965-shelby-cobra-superformance.jpeg",
-      model: "TVE",
-      price: 0,
-    },
-    {
-      image: "1965-shelby-cobra-superformance.jpeg",
-      model: "TVE",
-      price: 0,
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
 
   return (
     <div>
-      <div className="product-container">
-        {products.map((x) => (
-          <div className="product">
-            <img height="50px" width="60px" src={x.image} alt="" />
-            <p>Model: {x.model}</p>
-            <p>Price: {x.price > 0 ? "$ " + x.price : "Not available"} </p>
-          </div>
-        ))}
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((x) => {
+            return (
+              <tr>
+                <td>{x.id}</td>
+                <td>{x.title}</td>
+                <td>{x.category}</td>
+                <td>{x.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
